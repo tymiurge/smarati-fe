26a599a4cf154bfb6fba1fe306b679cf05585a84
tymiurge <tmrlabas@gmail.com>
2019-01-01 17:03:43 +0200

    initiating project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { CardExplorer } from './screens'
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <CardExplorer />
+      </>
+    );
+  }
+}
+
+export default App;

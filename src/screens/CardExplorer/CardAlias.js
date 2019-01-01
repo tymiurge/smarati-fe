b44410e9b3802fbc0c84f5c1e7614dc85719591a
tymiurge <tmrlabas@gmail.com>
2019-01-01 23:21:57 +0200

    CardAlias now supports onRemoveClick and onEditClick props

--- src/screens/CardExplorer/CardAlias.js
+++ src/screens/CardExplorer/CardAlias.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import { Segment, Header, Label, Icon } from 'semantic-ui-react'
 import { layouting, controls } from 'components'
+import PropTypes from 'prop-types'
+
+const propTypes = {
+  content: PropTypes.string.isRequired,
+  progress: PropTypes.number,
+  editMode: PropTypes.bool,
+  onEditClick: PropTypes.func.isRequired,
+  onRemoveClick: PropTypes.func.isRequired
+}
+
+const defaultProps = {
+  progress: 0,
+  onEditClick: () => {},
+  onRemoveClick: () => {}
+}
 
 const CardAlias = props => {
   const {
@@ -10,30 +25,36 @@ const CardAlias = props => {
   const editMode = props.editMode !== undefined ? props.editMode : false
   return (
     <div style={{width: '300px'}}>
-    {/* <div style={{height: '3px', width: '100%', backgroundColor: 'green'}}></div> */}
-    <controls.DashedProgress progress={progress} />
-    <Segment attached className='cursor-pointer'>
-      
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-      <Header as='h3' style={{maxWidth: '300px', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden'}}>{content}</Header>
-      </div>
-    </Segment>
+      <controls.DashedProgress progress={progress} />
 
-    <Segment attached style={{padding: '4px'}}>
-    <layouting.FullNFillInRow> 
-      <div> 
-        <Label color='blue' size='tiny'>Elliot</Label>
-        <Label color='blue' size='tiny'>linux</Label>
-      </div>
-      <Icon
-        name={editMode ? 'trash' : 'edit'}
-        color={editMode ? 'red' : 'blue'}
-        className='cursor-pointer'
-      />
-    </layouting.FullNFillInRow>
-    </Segment>
+      <Segment attached className='cursor-pointer'>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Header as='h3' style={{maxWidth: '300px', textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden'}}>{content}</Header>
+        </div>
+      </Segment>
+
+      <Segment attached style={{padding: '4px'}}>
+        <layouting.FullNFillInRow> 
+          <div> 
+            <Label color='blue' size='tiny'>Elliot</Label>
+            <Label color='blue' size='tiny'>linux</Label>
+          </div>
+          <Icon
+            name={editMode ? 'trash' : 'edit'}
+            color={editMode ? 'red' : 'blue'}
+            className='cursor-pointer'
+            onClick={() => {
+              if (editMode) return props.onRemoveClick()
+              return props.onEditClick()
+            }}
+          />
+        </layouting.FullNFillInRow>
+      </Segment>
     </div>
   )
 }
+
+CardAlias.propTypes = propTypes
+CardAlias.defaultProps = defaultProps
 
 export default CardAlias
--- src/screens/CardExplorer/CardExplorer.js
+++ src/screens/CardExplorer/CardExplorer.js
@@ -7,8 +7,16 @@ import { layouting } from 'components'
 const CardExplorer = props => {
   return (
     <div style={{display: 'flex', padding: '10px'}}>
-    <CardAlias content='abbreviation' progress={0} editMode />
-    <CardAlias content='go' progress={5}/>
+    <CardAlias
+      content='abbreviation' progress={0} editMode
+      onEditClick={() => alert('edit')}
+      onRemoveClick={() => alert('removing')}
+    />
+    <CardAlias
+      content='go' progress={5}
+      onEditClick={() => alert('edit')}
+      onRemoveClick={() => alert('removing')}
+    />
     <CardAlias content='docker restart mongoserver' progress={7}/>
     <CardAlias content='$ docker run --name some-mongo -d mongo:tag' progress={4} />
   </div>

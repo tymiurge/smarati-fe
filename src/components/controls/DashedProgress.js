214acef2c1e2cfd6933769fd60f0fb3e5c126c09
tymiurge <tmrlabas@gmail.com>
2019-01-02 11:50:13 +0200

    clearing warnings

--- src/components/controls/DashedProgress.js
+++ src/components/controls/DashedProgress.js
@@ -4,7 +4,7 @@ const DashedProgress = ({progress}) => (
   <div style={{height: '5px', display: 'flex', justifyContent: 'center'}}>
     {
       (new Array(7)).fill(0).map((_, key) => (
-        <div style={{height: '100%', flex: 1, paddingLeft: key !== 0 ? '1px' : '0px'}}>
+        <div key={key} style={{height: '100%', flex: 1, paddingLeft: key !== 0 ? '1px' : '0px'}}>
           <div style={{height: '100%', width: '100%', backgroundColor: key < progress ? 'green' : 'gray'}}></div>
         </div>    
       ))
--- src/screens/CardExplorer/CardExplorer.js
+++ src/screens/CardExplorer/CardExplorer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import CardAlias from './CardAlias'
-import CardBoxAlias from './CardBoxAlias'
 import { layouting } from 'components'
 
 const styles = {

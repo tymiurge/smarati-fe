4b27d9767c579c5a87609c7a76f4f2b206ec9094
tymiurge <tmrlabas@gmail.com>
2019-01-04 20:29:25 +0200

    CardAlias - selected skin look

--- src/screens/CardExplorer/CardAlias.js
+++ src/screens/CardExplorer/CardAlias.js
@@ -1,76 +1,95 @@
 import React from 'react'
-import { Segment, Header, Label, Icon } from 'semantic-ui-react'
+import { Segment, Header, Label, Icon, Checkbox, Dimmer } from 'semantic-ui-react'
 import { layouting, controls } from 'components'
 import PropTypes from 'prop-types'
 import { cardExplorer, withStyles } from 'component-styles'
 
+const StaticInfo = props => (
+  <Segment attached style={props.css.metaContainer}>
+    <layouting.FullNFillInRow> 
+      <div> 
+        <Label color='blue' size='tiny'>Elliot</Label>
+        <Label color='blue' size='tiny'>linux</Label>
+      </div>
+      <Icon
+        name={'edit'}
+        color='blue'
+        className='cursor-pointer'
+        onClick={props.onEditClick}
+      />
+    </layouting.FullNFillInRow>
+  </Segment>
+)
+
+const EditModeTools = props => (
+  <Segment attached style={props.css.metaContainer}>
+    <Checkbox
+      label='Select'
+      onChange={props.onSelectChange}
+      checked={props.checked}
+    />
+  </Segment>
+)
+
+const DimmerContainer = props => (
+  <Segment style={{padding: 0}}>
+    { props.children }
+  </Segment>
+)
+ 
 const propTypes = {
   content: PropTypes.string.isRequired,
   progress: PropTypes.number,
   editMode: PropTypes.bool,
   onEditClick: PropTypes.func.isRequired,
-  onRemoveClick: PropTypes.func.isRequired
 }
 
 const defaultProps = {
   progress: 0,
   onEditClick: () => {},
-  onRemoveClick: () => {}
 }
 
-const css1 = {
-  contentContainer: {
-    display: 'flex', 
-    justifyContent: 'center'
-  },
-  content: {
-    maxWidth: '300px', 
-    textOverflow: 'ellipsis', 
-    whiteSpace: 'nowrap', 
-    overflow: 'hidden'
-  },
-  metaContainer: {
-    padding: '4px'  
+class CardAlias extends React.Component {
+  state = {
+    selected: false
   }
-}
-
-const CardAlias = props => {
-  const {
-    content,
-    progress,
-    css
-  } = props
-  const editMode = props.editMode !== undefined ? props.editMode : false
-  const containerStyle = props.containerStyle || {}
-  return (
-    <div style={{ ...containerStyle}}>
-      <controls.DashedProgress progress={progress} />
-
-      <Segment attached className='cursor-pointer'>
-        <div style={css.contentContainer}>
-          <Header as='h3' style={css.content}>{content}</Header>
-        </div>
-      </Segment>
-
-      <Segment attached style={css.metaContainer}>
-        <layouting.FullNFillInRow> 
-          <div> 
-            <Label color='blue' size='tiny'>Elliot</Label>
-            <Label color='blue' size='tiny'>linux</Label>
+  render() {
+    const { props, state } = this
+    const {
+      content,
+      progress,
+      css
+    } = props
+    const editMode = props.editMode !== undefined ? props.editMode : false
+    const containerStyle = props.containerStyle || {}
+    return (
+      <div style={{ ...containerStyle}}>
+        <Dimmer.Dimmable as={DimmerContainer} dimmed={true}>
+        <controls.DashedProgress progress={progress} />
+        <Segment attached className='cursor-pointer'>
+          <div style={css.contentContainer}>
+            <Header as='h3' style={css.content}>{content}</Header>
           </div>
-          <Icon
-            name={editMode ? 'trash' : 'edit'}
-            color={editMode ? 'red' : 'blue'}
+        </Segment>
+        {
+          editMode 
+            ? <EditModeTools {...props} onSelectChange={() => alert('changing')} checked={state.selected} />
+            : <StaticInfo {...props} />
+        }
+          <Dimmer
+            active={true}
+            onClickOutside={() => alert('ok')}
             className='cursor-pointer'
-            onClick={() => {
-              if (editMode) return props.onRemoveClick()
-              return props.onEditClick()
-            }}
-          />
-        </layouting.FullNFillInRow>
-      </Segment>
-    </div>
-  )
+          >
+            <Header as='h5' icon inverted>
+              <Icon name='check' />
+              Click To Uncheck
+            </Header>
+          </Dimmer>
+        </Dimmer.Dimmable>
+      </div>  
+    )
+  }
 }
 
 CardAlias.propTypes = propTypes
